207d3baf41c400868c8a4a3ebf4f77d5241ee7ab
Sebastian Sanokowski <sebastian.sanokowski@tum.de>
2026-01-17 15:36:00 +0100

    update

--- READMEs/Clusters/Vega/CustoMReadme.md
+++ READMEs/Clusters/Vega/CustoMReadme.md
@@ -14,4 +14,5 @@ flag --account
 ```
 sbatch ./READMEs/Clusters/Vega/run_gpu_dime_control.sh
 sbatch ./READMEs/Clusters/Vega/run_gpu_dime_low_dim.sh
+sbatch ./READMEs/Clusters/Vega/run_gpu_WPO_hyper.sh
 ```
--- READMEs/Sweeps/run_sweep_dmerl_WPO_hyper.sh
+++ READMEs/Sweeps/run_sweep_dmerl_WPO_hyper.sh
@@ -59,7 +59,7 @@ for ENV_NAME in "${ENV_NAMES[@]}"; do
         echo "Starting env.name=$ENV_NAME (lr=$LR) on GPU $GPU_ID..."
         CUDA_VISIBLE_DEVICES=$GPU_ID python -m src.jaxrl.reppo_DMERL_new \
             env.name="$ENV_NAME" \
-            wandb.project_suffix="_FR_16_01" \
+            wandb.project_suffix="_WPO_hyper" \
             hyperparameters.num_eval=50 \
             hyperparameters.total_time_steps=50000000 \
             hyperparameters.diffusion.diff_steps=8 \
