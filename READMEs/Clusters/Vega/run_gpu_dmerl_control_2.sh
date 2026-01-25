0f3701abbc784d482a71d0734d2d910d5c811945
Sebastian Sanokowski <sebastian.sanokowski@tum.de>
2026-01-25 23:05:46 +0100

    update

--- READMEs/Clusters/Vega/run_gpu_dmerl_control_2.sh
+++ READMEs/Clusters/Vega/run_gpu_dmerl_control_2.sh
@@ -18,5 +18,5 @@ which python
 python -c "import sys; print(sys.executable)"
 python -c "import jax; print(jax.__file__)"
 # run your code
-chmod +x ./READMEs/Sweeps/DMERL/vanilla/run_sweep_dmerl_control.sh
-sh ./READMEs/Sweeps/DMERL/vanilla/run_sweep_dmerl_control.sh
+chmod +x ./READMEs/Sweeps/DMERL/vanilla/run_sweep_dmerl_control_2.sh
+sh ./READMEs/Sweeps/DMERL/vanilla/run_sweep_dmerl_control_2.sh
