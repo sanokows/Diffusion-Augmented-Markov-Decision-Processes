e416a97df48dbd55b1e0c0683bcaf273498f77ec
Sebastian Sanokowski <sebastian.sanokowski@tum.de>
2026-03-08 21:52:58 +0100

    update

--- READMEs/Sweeps/DMERL/vanilla/more_steps/Final_Runs/run_sweep_4_steps_tests.sh
+++ READMEs/Sweeps/DMERL/vanilla/more_steps/Final_Runs/run_sweep_4_steps_tests.sh
@@ -13,9 +13,9 @@ DIFF_STEP=4
 # Format: "v_value|lr|temperature_lr|lagrangian_lr|aux_loss_mult|gamma|lmbda|num_mini_batches|ent_target_mult|num_collection_step_factor"
 SWEEP_TUPLES=(
     "10|1e-3|3e-4|3e-4|0.25|0.9908|0.95|4|2.2|0.5" ### this works best
-    "10|1e-3|3e-4|3e-4|0.25|0.9908|0.95|4|2.2|1"
-    "10|1e-3|3e-4|3e-4|0.25|0.9908|0.96|4|2.2|0.5"
-    "10|1e-3|3e-4|3e-4|0.15|0.9908|0.95|4|2.2|0.5"
+    "10|1e-3|3e-4|3e-4|0.25|0.9908|0.96|4|2.2|1"
+    "10|1e-3|3e-4|3e-4|0.35|0.9908|0.96|4|2.2|0.5"
+    "10|1e-3|3e-4|3e-4|0.15|0.9908|0.96|4|2.2|0.5"
 )
 
 
