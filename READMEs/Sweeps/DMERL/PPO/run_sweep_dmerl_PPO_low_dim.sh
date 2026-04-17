317f4b703ccb704d097e28c56b02b1599efee05a
Sebastian Sanokowski <sebastian.sanokowski@tum.de>
2026-04-17 09:39:43 +0200

    update

--- READMEs/Sweeps/DMERL/PPO/run_sweep_dmerl_PPO_low_dim.sh
+++ READMEs/Sweeps/DMERL/PPO/run_sweep_dmerl_PPO_low_dim.sh
@@ -47,8 +47,8 @@ for ENV_NAME in "${ENV_NAMES[@]}"; do
         DiffPPO_overrides=default \
         env=mjx_dmc \
         wandb.project_suffix="_FR_PPO_14_04" \
-        seed=0 \
-        trials=2 &
+        trials=6 \
+        seed=2 &
     GPU_PIDS[$GPU_ID]=$!
     GPU_INDEX=$((GPU_INDEX + 1))
 done
