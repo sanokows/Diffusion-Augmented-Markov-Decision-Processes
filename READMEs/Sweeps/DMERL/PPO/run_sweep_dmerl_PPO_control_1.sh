67904503f45d3b4e16b8a9fb4140f93e167b53e1
Sebastian Sanokowski <sebastian.sanokowski@tum.de>
2026-04-20 15:25:27 +0200

    update for PPO reruns on updated conda env

--- READMEs/Sweeps/DMERL/PPO/run_sweep_dmerl_PPO_control_1.sh
+++ READMEs/Sweeps/DMERL/PPO/run_sweep_dmerl_PPO_control_1.sh
@@ -46,9 +46,9 @@ for ENV_NAME in "${ENV_NAMES[@]}"; do
         env.name="$ENV_NAME" \
         DiffPPO_overrides=default \
         env=mjx_dmc \
-        wandb.project_suffix="_FR_PPO_14_04" \
-        trials=6 \
-        seed=2 &
+        wandb.project_suffix="_FR_PPO_20_04" \
+        trials=4 \
+        seed=0 &
     GPU_PIDS[$GPU_ID]=$!
     GPU_INDEX=$((GPU_INDEX + 1))
 done
