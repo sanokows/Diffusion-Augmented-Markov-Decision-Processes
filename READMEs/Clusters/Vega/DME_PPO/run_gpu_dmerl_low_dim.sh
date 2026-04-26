c2b88d1ce033225d98220226669f965c8b64fe1d
Sebastian Sanokowski <sebastian.sanokowski@tum.de>
2026-04-26 10:27:28 +0200

    update

---
+++ READMEs/Clusters/Vega/DME_PPO/run_gpu_dmerl_low_dim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name="dmerl_control_2"
+#SBATCH --time=42:00:00
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=gpu
+#SBATCH --mem=16GB
+#SBATCH --account=d2025d09-019-users
+#SBATCH --signal=INT@60
+
+# conda activate REPPO
+# cd /ceph/hpc/home/eusebastians/code/DMERL
+
+which python
+python -c "import sys; print(sys.executable)"
+python -c "import jax; print(jax.__file__)"
+# run your code
+chmod +x ./READMEs/Sweeps/DMERL/PPO/run_sweep_dmerl_PPO_low_dim.sh
+sh ./READMEs/Sweeps/DMERL/PPO/run_sweep_dmerl_PPO_low_dim.sh
